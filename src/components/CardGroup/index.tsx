9a159c1409c5fbb5ad570f3687fe15c99c7fd42e
Davi Santos <daviir17@gmail.com>
2025-06-05 01:28:11 -0300

    tipagem e conexão com websocker do servidor

--- src/components/CardGroup/index.tsx
+++ src/components/CardGroup/index.tsx
@@ -1,6 +1,6 @@
 export default function CardGroup() {
   return (
-    <div className="p-2">
+    <div className="p-2 cursor-pointer">
       <div className="flex items-center justify-between p-4 bg-white rounded-xl shadow-md hover:shadow-lg transition">
         <p className="font-semibold text-gray-900">Ankit Mishra</p>
         <p className="text-sm text-gray-500 truncate max-w-[160px]">
--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,9 +1,67 @@
 import CardGroup from '@/components/CardGroup';
 import InputSendMessage from '@/components/InputSendMessage';
 import Layout from '@/components/Layout';
+import { Button } from '@/components/ui/button';
 import clsx from 'clsx';
+import { Plus, Send } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+
+interface Message {
+  room: string;
+  text: string;
+  createdAt: string; // vindo como string do back-end (JSON)
+  username: string;
+}
+
+interface SelectRoomData {
+  username: string;
+  room: string;
+}
+
+const socket = io('http://localhost:3000');
 
 export default function Chat() {
+  const [room, setRoom] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [joined, setJoined] = useState<boolean>(false);
+
+  function handleJoinRoom(): void {
+    if (username !== '' && room !== '') {
+      socket.emit(
+        'select_room',
+        { username, room },
+        (roomMessages: Message[]) => {
+          setMessages(roomMessages);
+          setJoined(true);
+        }
+      );
+    }
+  }
+
+  const handleSendMessage = () => {
+    if (message.trim() !== '') {
+      socket.emit('message', {
+        room,
+        message,
+        username,
+      });
+      setMessage('');
+    }
+  };
+
+  useEffect(() => {
+    socket.on('message', (newMessage) => {
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    });
+
+    return () => {
+      socket.off('message');
+    };
+  }, []);
+
   return (
     <Layout>
       <div
@@ -20,13 +78,20 @@ export default function Chat() {
         )}
       >
         <div className="w-2/3 bg-white border-r-1">
-          <div className="h-16 flex items-center pl-2 border-b-2">
+          <div className="h-16 flex items-center justify-between pl-2 pr-2 border-b-2">
             <h1 className="font-extrabold text-slate-900 text-2xl">Grupos</h1>
+            <Button
+              type="button"
+              variant="outline"
+              className="cursor-pointer"
+            >
+              <Plus/>
+            </Button>
           </div>
-          <CardGroup/>
-          <CardGroup/>
-          <CardGroup/>
-          <CardGroup/>
+          <CardGroup />
+          <CardGroup />
+          <CardGroup />
+          <CardGroup />
         </div>
         <div className="w-full h-full flex flex-col">
           <div className="h-16 flex items-center shrink-0 pl-2  border-b-2  bg-white">
